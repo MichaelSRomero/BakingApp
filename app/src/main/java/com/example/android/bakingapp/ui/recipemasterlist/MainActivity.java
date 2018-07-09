67e3a7192bd601e66190bfedf586ee0c1c2aeff4
MichaelSRomero <liikemike@aim.com>
2018-07-09 15:48:14 -0400

    Deleted Fragment Master List xml & java file, moved all code to MainActivity

--- app/src/main/java/com/example/android/bakingapp/ui/recipemasterlist/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/ui/recipemasterlist/MainActivity.java
@@ -1,17 +1,50 @@
 package com.example.android.bakingapp.ui.recipemasterlist;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.model.Recipe;
+import com.example.android.bakingapp.viewmodel.RecipeViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private List<Recipe> mRecipeList;
+    private MasterListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.master_recycler_view);
+
+        mRecipeList = new ArrayList<>();
+        mAdapter = new MasterListAdapter(this, mRecipeList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(mAdapter);
+
+        setupViewModel();
+    }
+
+    private void setupViewModel() {
+        RecipeViewModel model = ViewModelProviders.of(this).get(RecipeViewModel.class);
+        model.getRecipeLists().observe(this, new Observer<List<Recipe>>() {
+            @Override
+            public void onChanged(@Nullable List<Recipe> recipes) {
+                mAdapter.addData(recipes);
+            }
+        });
     }
 }
